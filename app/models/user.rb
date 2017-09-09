e2951480b786dbe45cf3bf16312a749dfe433154
Mateusz Kaczmarek <mk16939@gmail.com>
2017-09-09 14:32:05 +0000

    Finish toy app without errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
-    validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+    validates :name, presence: true
+    validates :email, presence: true
 end
